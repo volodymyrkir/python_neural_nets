7ee57fa2f8a4680fc8b9540b78b12ecacdc79279
volodymyr <zalorderon3331@gmail.com>
2023-11-11 22:09:58 +0200

    refactoring

--- tasks/task_3_core/node.py
+++ tasks/task_3_core/node.py
@@ -1,7 +1,10 @@
+"""This is a helper module for SOM nodes"""
+
+
 class Node:
     def __init__(self, index, index_normalized, position):
         self.index = index
-        self.index_normalized = position
+        self.index_normalized = index_normalized
         self.position = position
         self.neighbors = []
 
--- tasks/task_3_core/som.py
+++ tasks/task_3_core/som.py
@@ -1,11 +1,15 @@
-from task_3_core.node import Node
+"""This is a helper module that contains SOM model definition"""
+import random
+import math
 from itertools import chain
 
 import numpy as np
-import random
-import math
+
+from tasks.task_3_core.node import Node
+
 
 class SOM:
+
     def get_neighbors_by_index(self, node):
         neighbors = []
 
@@ -21,27 +25,26 @@ class SOM:
             x, y = position
 
             if x < 0 or x >= self.shape[0]:
-                 continue
-            
+                continue
+
             if y < 0 or y >= self.shape[1]:
-                 continue
-            
+                continue
+
             neighbors.append(self.nodes[x][y])
 
         return neighbors
 
     def initialize_nodes(self):
         self.nodes = []
-        
         for x in range(self.shape[0]):
             row = []
-            
+
             for y in range(self.shape[1]):
                 index = np.array((x, y))
                 index_norm = index / (max(self.shape) - 1)
                 position = np.pad(index_norm, (0, self.input_shape - 2), 'constant', constant_values=0)
 
-                node = Node(index,  index_norm, position)
+                node = Node(index, index_norm, position)
                 row.append(node)
 
             self.nodes.append(row)
@@ -51,7 +54,7 @@ class SOM:
                 neighbors = self.get_neighbors_by_index(node)
 
                 for neighbor in neighbors:
-                        node.add_neighbor(neighbor)
+                    node.add_neighbor(neighbor)
 
         self.nodes = list(chain.from_iterable(self.nodes))
 
@@ -63,13 +66,18 @@ class SOM:
     def __init__(self, shape, input_shape, a0=0.8, s0=0.5):
         self.a0 = a0
         self.s0 = s0
-        
+
         self.shape = shape
         self.input_shape = input_shape
 
         self.initialize_nodes()
 
-    def distance_function_sq(self, p1, p2):
+        self.total_distance = 0
+        self.erroneous_nodes_count = 0
+        self.iterations = 0
+
+    @staticmethod
+    def distance_function_sq(p1, p2):
         point1 = np.array(p1)
         point2 = np.array(p2)
 
@@ -86,8 +94,8 @@ class SOM:
     def sigma(self, iteration, max_iterations):
         return self.s0 * math.exp(- iteration / max_iterations)
 
-    # Cosine function
     def neighborhood_function(self, bmu, node, iteration, max_iterations):
+        """Cosine function"""
         s = self.sigma(iteration, max_iterations)
         d = self.distance_function_sq(bmu, node)
 
@@ -97,23 +105,23 @@ class SOM:
         self.initialize_nodes()
         self.reset_callbacks()
 
-        while(self.iterations < max_iterations):
+        while self.iterations < max_iterations:
             example = random.choice(training_data)
-            nodes_sorted = sorted(self.nodes, key = lambda n: self.distance_function_sq(n.position, example))
+            nodes_sorted = sorted(self.nodes, key=lambda n: self.distance_function_sq(n.position, example))
             bmu, sbmu = nodes_sorted[:2]
 
-            # Callbacks
+            not_sbmu = not sbmu
             self.total_distance += self.distance_function_sq(bmu.position, example)
-            if not sbmu in bmu.neighbors:
-               self. erroneous_nodes_count += 1
+            if not_sbmu in bmu.neighbors:
+                self.erroneous_nodes_count += 1
 
             bmu_pos = bmu.index_normalized
 
             for node in self.nodes:
                 node.position = node.position + (
-                    self.neighborhood_function(bmu_pos, node.index_normalized, self.iterations, max_iterations) 
-                    * self.alpha(self.iterations, max_iterations) 
-                    * (example - node.position)
+                        self.neighborhood_function(bmu_pos, node.index_normalized, self.iterations, max_iterations)
+                        * self.alpha(self.iterations, max_iterations)
+                        * (example - node.position)
                 )
 
             self.iterations += 1
