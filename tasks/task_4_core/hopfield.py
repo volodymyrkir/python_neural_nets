1a73079e9f98464de2ef4f800ca41ba54a97c4cb
volodymyr <zalorderon3331@gmail.com>
2023-11-16 03:02:25 +0200

    refactoring + requirements task 4

--- tasks/task_4_core/hopfield.py
+++ tasks/task_4_core/hopfield.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Hopfield:
     def __init__(self, shape):
         self.neurons_count = shape[0] * shape[1]
@@ -7,7 +8,7 @@ class Hopfield:
     def train_weights(self, train_data, learning_rule='hebb'):
         self.train_data = np.array(train_data)
         examples_count = len(train_data)
-        
+
         # initialize weights
         W = np.zeros((self.neurons_count, self.neurons_count))
         rho = np.sum([np.sum(t) for t in train_data]) / (examples_count * self.neurons_count)
@@ -15,7 +16,7 @@ class Hopfield:
         if learning_rule == 'hebb':
             # Hebbian rule
             for t in self.train_data:
-                t = t - rho            
+                t = t - rho
                 W += np.outer(t, t)
 
             # Make diagonal element of W into 0
@@ -23,6 +24,7 @@ class Hopfield:
             np.fill_diagonal(W, 0)
 
             self.W = W
+
         elif learning_rule == 'storkey':
             # Storkey rule
             for t in self.train_data:
@@ -37,22 +39,24 @@ class Hopfield:
             np.fill_diagonal(W, 0)
 
             self.W = W
+
         elif learning_rule == 'demircigil':
             pass
         else:
-            raise Exception("Not supported update function") 
-         
-    
-    def predict(self, data, iterations=20, threshold=0, energy_function='classic', update_function='sync', async_iterations=100):
+            raise ValueError("Not supported update function")
+
+    def predict(self, data, iterations=20, threshold=0,
+                energy_function='classic', update_function='sync', async_iterations=100):
         # Copy to avoid call by reference 
         copied_data = np.copy(data)
-        
+
         # Define predict list
         predicted = []
         for i in range(len(data)):
-            predicted.append(self.update(copied_data[i], iterations, threshold, energy_function, update_function, async_iterations))
+            predicted.append(
+                self.update(copied_data[i], iterations, threshold, energy_function, update_function, async_iterations))
         return predicted
-    
+
     def update_async(self, initial_state, iterations, threshold, energy_closure, async_iterations):
         """
         Asynchronous update
@@ -60,25 +64,25 @@ class Hopfield:
         # Compute initial state energy
         state = initial_state
         e = energy_closure(state)
-        
+
         # Iteration
         for i in range(iterations):
             for j in range(async_iterations):
                 # Select random neuron
-                index = np.random.randint(0, self.neurons_count) 
+                index = np.random.randint(0, self.neurons_count)
                 # Update state
                 state[index] = np.sign(self.W[index].T @ state - threshold)
-            
+
             # Compute new state energy
             e_new = energy_closure(state)
-            
+
             # state is converged
             if e == e_new:
                 return state
             # Update energy
             e = e_new
         return state
-    
+
     def update_sync(self, initial_state, iterations, threshold, energy_closure):
         """
         Synchronous update
@@ -87,21 +91,21 @@ class Hopfield:
         state = initial_state
 
         e = energy_closure(state)
-        
+
         # Iteration
         for i in range(iterations):
             # Update state
             state = np.sign(self.W @ state - threshold)
             # Compute new state energy
             e_new = energy_closure(state)
-            
+
             # state is converged
             if e == e_new:
                 return state
             # Update energy
             e = e_new
         return state
-    
+
     def update_demircigil(self, initial_state, iterations, threshold, energy_closure):
         """
         Demircigil asynchronous update
@@ -109,7 +113,7 @@ class Hopfield:
         # Compute initial state energy
         state = initial_state
         e = energy_closure(state)
-        
+
         def update_neuron(l):
             # Get state with selected neuron flipped
             state_flipped_pos = np.copy(state)
@@ -120,6 +124,7 @@ class Hopfield:
 
             # Update state
             return np.sign(-energy_closure(state_flipped_pos) + energy_closure(state_flipped_neg))
+
         next = np.vectorize(update_neuron)
 
         # Iteration
@@ -128,7 +133,7 @@ class Hopfield:
 
             # Compute new state energy
             e_new = energy_closure(state)
-            
+
             # state is converged
             if e == e_new:
                 return state
@@ -139,7 +144,7 @@ class Hopfield:
     def update(self, initial_state, iterations, threshold, energy_function, update_function, async_iterations):
         def energy_closure(state):
             return self.energy(state, threshold, energy_function)
-        
+
         if update_function == 'async':
             return self.update_async(initial_state, iterations, threshold, energy_closure, async_iterations)
         elif update_function == 'sync':
@@ -147,8 +152,8 @@ class Hopfield:
         elif update_function == 'demircigil':
             return self.update_demircigil(initial_state, iterations, threshold, energy_closure)
         else:
-            raise Exception("Not supported update function") 
-    
+            raise Exception("Not supported update function")
+
     def energy_classic(self, state, threshold):
         return -0.5 * state @ self.W @ state + np.sum(state * threshold)
 
