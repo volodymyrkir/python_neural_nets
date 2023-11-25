c5224aba876244d005271cd471f54cbaa940ce24
Alexiush <streltsov2609@gmail.com>
2023-11-25 19:04:11 +0200

    Task 5 initial commit

---
+++ tasks/task_5_core/lvq.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+class LVQ:
+    def __init__(self):
+        pass
+
+    def distance(self, first, second):
+        return np.sqrt(np.sum(np.square(first - second), axis=1))
+
+    def update_two_step(self, train_data):
+        for features, label in zip(train_data[0], train_data[1]):
+            distances = self.distance(self.patterns, features)
+
+            min_idx, right_idx = 0, 0
+            for i in range(distances.shape[0]):
+                if distances[i] < distances[min_idx]:
+                    min_idx = i
+
+                if distances[i] < distances[right_idx] and self.labels[i] == label:
+                    right_idx = i
+
+            if min_idx == right_idx:
+                bmu_w = self.patterns[min_idx]
+                error = (features - bmu_w)
+                bmu_w += error * self.alpha
+            else:
+                bmu_w = self.patterns[min_idx]
+                error_bmu = (features - bmu_w)
+                bmu_w -= error_bmu * self.alpha
+
+                right_w = self.patterns[right_idx]
+                error = (features - right_w)
+                right_w += error * self.alpha
+
+    def update_classic(self, train_data):
+        for features, label in zip(train_data[0], train_data[1]):
+            min_idx = np.argmin(self.distance(features, self.patterns))
+            bmu_w, bmu_l = self.patterns[min_idx], self.labels[min_idx]
+            error = (features - bmu_w)
+
+            if bmu_l == label:
+                bmu_w += error * self.alpha
+            else:
+                bmu_w -= error * self.alpha
+
+    def train(self, init_data, train_data, learning_rule='classic', alpha=0.01, epochs=1):
+        self.alpha = alpha
+        self.patterns = init_data[0]
+        self.labels = init_data[1]
+        
+        for _ in range(epochs):
+            if learning_rule == 'classic':
+                self.update_classic(train_data)
+            elif learning_rule == 'two_step':
+                self.update_two_step(train_data)
+            else:
+                raise ValueError("Not supported train function")
+
+    def predict(self, entries):
+        return list(map(lambda entry: self.labels[np.argmin(self.distance(entry, self.patterns))], entries))
